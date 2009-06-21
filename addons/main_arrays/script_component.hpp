9cef248eadb7fdff17c153930301e64f4ab91c72
Sickboy <sb@dev-heaven.net>
2009-06-21 14:42:01 +0200

    Added skeleton for arrays, eventhandlers, hashes, network, strings

---
+++ addons/main_arrays/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT main_arrays
+#include "\x\cba\addons\main\script_macros.hpp"
+
+#ifdef DEBUG_MAIN_ARRAYS
+	#define DEBUG
+#endif
+
+#ifdef DEBUG_MAIN_ARRAYS_SETTINGS
+	#define DEBUG_SETTINGS DEBUG_MAIN_ARRAYS_SETTINGS
+#endif
+#ifndef DEBUG_MAIN_ARRAYS_SETTINGS
+	#define DEBUG_SETTINGS DEFAULT_DEBUG_SETTINGS
+#endif
