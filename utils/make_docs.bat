6df174e93a57526e05ad8be20a4e7f465652c3fb
Spooner <spooner@dev-heaven.net>
2009-06-24 13:19:23 +0100

    REMOVED: docs folder. ADDED: Replaced by function_library.7z

--- utils/make_docs.bat
+++ utils/make_docs.bat
@@ -1,3 +1,12 @@
 @echo off
 
-perl "C:\Program Files (x86)\NaturalDocs-1.4\NaturalDocs" -i "..\addons" -o HTML "..\doc" -p "ndocs-project" -s Default cba
+echo === Compiling documentation ===
+echo.
+perl "C:\Program Files (x86)\NaturalDocs-1.4\NaturalDocs" -i "..\addons" -o HTML "..\docs\function_library" -p "ndocs-project" -s Default cba
+
+echo.
+echo === Packaging documentation ===
+echo.
+7z a "..\docs\function_library"
+
+pause
