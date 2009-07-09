5827cbee9be00858abf0db4c11ca68d55e88301a
Sickboy <sb@dev-heaven.net>
2009-07-09 15:31:02 +0200

    Functions are precompiled in common, so no need for postInit

--- addons/diagnostic/CfgEventHandlers.hpp
+++ addons/diagnostic/CfgEventHandlers.hpp
@@ -6,10 +6,3 @@ class Extended_PreInit_EventHandlers
 	};
 };
 
-class Extended_PostInit_EventHandlers
-{
-	class ADDON
-	{
-		init = QUOTE(CALLF(XEH_PostInit_Once));
-	};
-};
--- addons/diagnostic/XEH_PostInit_Once.sqf
+++
@@ -1,3 +0,0 @@
-#include "script_component.hpp"
-
-[GVAR(debug), { _this CALLMAIN(debug) }] CALLMAIN(addEventHandler);
