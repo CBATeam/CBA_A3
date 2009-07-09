edb90c2370c7f32467aef22efabfb80d6435949d
Sickboy <sb@dev-heaven.net>
2009-07-10 00:03:48 +0200

    Fixed: version number

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,7 +1,7 @@
 // COMPONET should be defined in the script_component.hpp and included BEFORE this hpp
 #define PREFIX cba
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
-#define VERSION 0.0101
+#define VERSION 0.0102
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.02
 
