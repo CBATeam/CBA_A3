7ab910ad68a8306de5f2ab5f18db8ea8348d8984
Sickboy <sb@dev-heaven.net>
2010-01-04 18:07:41 +0100

    Build

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -1,7 +1,8 @@
 // COMPONENT should be defined in the script_component.hpp and included BEFORE this hpp
 #define PREFIX cba
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
-#define VERSION 0.0103
+#define BUILD 205
+#define VERSION 0.2.BUILD
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.02
 
