8d53473897982a4dcb75a8c103524ccd1bdaf79c
Killswitch <killswitch@zeus-community.net>
2010-01-22 16:57:04 +0100

    Version bumpage - XEH now at 2.05

--- addons/extended_eventhandlers/CfgPatches.hpp
+++ addons/extended_eventhandlers/CfgPatches.hpp
@@ -4,7 +4,7 @@ class CfgPatches
 	{
 		units[] = {};
 		requiredVersion = 1.00;
-		SLX_XEH2_Version = 2.04;
+		SLX_XEH2_Version = 2.05;
 		requiredAddons[] =
 		{
 			//"CAData", "CAAir", "CACharacters"
