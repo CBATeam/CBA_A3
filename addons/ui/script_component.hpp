8702aac446e410afd199da3127f9505cb4783bb6
Sickboy <sb@dev-heaven.net>
2010-02-09 10:33:31 +0100

    Prepared CBA_ui

---
+++ addons/ui/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT ui
+#include "\x\cba\addons\main\script_mod.hpp"
+
+
+#ifdef DEBUG_ENABLED_UI
+	#define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_UI
+	#define DEBUG_SETTINGS DEBUG_SETTINGS_UI
+#endif
+
+#include "\x\cba\addons\main\script_macros.hpp"
