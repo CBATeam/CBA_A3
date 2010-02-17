202c8f44fa37173d7304042c287ddf67b796cf67
Sickboy <sb@dev-heaven.net>
2010-02-17 17:29:46 +0100

    debugging

--- addons/ui/script_component.hpp
+++ addons/ui/script_component.hpp
@@ -17,4 +17,5 @@
 #define PATHTO_SUB(var1,var2,var3,var4) MAINPREFIX\##var1\SUBPREFIX\##var2\##var3\##var4.sqf
 #define COMPILE_FILE_SUB(var1,var2,var3,var4) compile preProcessFileLineNumbers 'PATHTO_SUB(var1,var2,var3,var4)'
 #define PREP_SYS_SUB(var1,var2,var3,var4) ##var1##_##var2##_fnc_##var4 = COMPILE_FILE_SUB(var1,var2,var3,DOUBLES(fnc,var4))
-#define PREP_SUB(var1,var2) PREP_SYS_SUB(PREFIX,COMPONENT_F,var1,var2); diag_log [QUOTE(DOUBLES(DOUBLES(PREFIX,COMPONENT_F),DOUBLES(fnc,var2))),'=',QUOTE(MAINPREFIX\PREFIX\SUBPREFIX\COMPONENT_F\##var1\##var2.sqf)]
+#define PREP_SUB(var1,var2) PREP_SYS_SUB(PREFIX,COMPONENT_F,var1,var2)
+//#define PREP_SUB(var1,var2) PREP_SYS_SUB(PREFIX,COMPONENT_F,var1,var2); diag_log [QUOTE(DOUBLES(DOUBLES(PREFIX,COMPONENT_F),DOUBLES(fnc,var2))),'=',QUOTE(MAINPREFIX\PREFIX\SUBPREFIX\COMPONENT_F\##var1\##var2.sqf)]
