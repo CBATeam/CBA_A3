922418eca8ad8877bec63948d3140517a374b391
Sickboy <sb@dev-heaven.net>
2010-03-04 19:54:17 +0100

    build

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,8 +3,8 @@
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 0
 #define MINOR 3
-#define PATCHLVL 0
-#define BUILD 80
+#define PATCHLVL 1
+#define BUILD 82
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
