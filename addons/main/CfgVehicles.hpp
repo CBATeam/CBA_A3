30a3943b9f914c6399efb1cb38b063d3b4725e0f
Sickboy <sb@dev-heaven.net>
2010-04-24 15:00:10 +0200

    Changed: Removed CBA_logics vehicleclass, and moved "Require CBA" logic to Modules

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -1,20 +1,11 @@
 // Add a game logic which does nothing except requires the addon in the mission.
 
-class CfgVehicleClasses
-{
-	class CBA_logics
-	{
-		displayName = "CBA";
-	};
-};
-
 class CfgVehicles
 {
-	class Logic;
-	
+	class Logic;	
 	class CBA_main_require : Logic
 	{
 		displayName = "Require CBA";
-		vehicleClass = "CBA_logics";
+		vehicleClass = "Modules";
 	};
 };
