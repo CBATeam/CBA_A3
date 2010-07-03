963656df078ba0e070910915be292b6229e31122
Sickboy <sb@dev-heaven.net>
2010-07-03 19:04:53 +0200

    Fixed: InitPost missing

--- addons/diagnostic/xeh/config.cpp
+++ addons/diagnostic/xeh/config.cpp
@@ -1,4 +1,3 @@
-// TODO: server##type client##type ?
 // #define EH_DEBUG_ENABLED
 #ifdef EH_DEBUG_ENABLED
 	#include "script_component.hpp"
@@ -17,12 +16,16 @@
 	#define EH_INIT(type2) ##type2 = QUOTE(FUNC(diag_xeh) = { diag_log [ARGUMENTS]; }; ['type type2'] call FUNC(diag_xeh))
 	
 	#define EH_DEBUG(type) class Extended_##type##_EventHandlers { class All { class 0 { ##type = QUOTE([UNIT_ARGS] call FUNC(diag_xeh)); }; }; }
+	// Exception: INIT and INITPOST both use init, serverInit, clientInit :|
+	// TODO: serverInit/clientInit ?
+	#define EH_DEBUG_INIT(type) class Extended_##type##_EventHandlers { class All { class 0 { init = QUOTE([UNIT_ARGS] call FUNC(diag_xeh)); }; }; }
+	// TODO: server##type client##type ?
 	#define EH_DEBUG_ONCE(type) class Extended_##type##_EventHandlers { class 0 { EH_INIT(init); EH_INIT(serverInit); EH_INIT(clientInit); }; }
 
 	EH_DEBUG_ONCE(preInit);
 	EH_DEBUG_ONCE(postInit);
-	EH_DEBUG(InitPost);
-	EH_DEBUG(Init);
+	EH_DEBUG_INIT(InitPost);
+	EH_DEBUG_INIT(Init);
 	EH_DEBUG(AnimChanged);
 	EH_DEBUG(AnimDone);
 	EH_DEBUG(Dammaged);
