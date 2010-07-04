dd7536c7d6e5a866ccec12f11fac65e1bcd4c5a8
Sickboy <sb@dev-heaven.net>
2010-07-04 12:57:07 +0200

    Bumped requiredVersion

--- addons/extended_eventhandlers/CfgPatches.hpp
+++ addons/extended_eventhandlers/CfgPatches.hpp
@@ -3,7 +3,7 @@ class CfgPatches
 	class Extended_EventHandlers
 	{
 		units[] = {};
-		requiredVersion = 1.00;
+		requiredVersion = 1.50;
 		SLX_XEH2_Version = 2.06;
 		author[] = {"Solus", "Killswitch", "Sickboy"};
 		authorUrl = "http://dev-heaven.net/projects/cca";
