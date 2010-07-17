e5f8782f5995e81217782aef78fff9b8d6146345
Killswitch <killswitch@zeus-community.net>
2010-07-17 11:52:56 +0200

    XEH version bump to 3.02

--- addons/extended_eventhandlers/CfgPatches.hpp
+++ addons/extended_eventhandlers/CfgPatches.hpp
@@ -4,7 +4,7 @@ class CfgPatches
 	{
 		units[] = {};
 		requiredVersion = 1.50;
-		SLX_XEH2_Version = 3.01;
+		SLX_XEH2_Version = 3.02;
 		author[] = {"Solus", "Killswitch", "Sickboy"};
 		authorUrl = "http://dev-heaven.net/projects/cca";
 		requiredAddons[] =
