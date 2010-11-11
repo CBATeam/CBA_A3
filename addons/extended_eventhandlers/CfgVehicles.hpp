f18bbbb4d3f13f33b0eb61f2f65de6be49cf526d
Sickboy <sb@dev-heaven.net>
2010-11-11 10:47:27 +0100

    ~ ADDED: SLX_XEH_Logic (Module); (Backup) initialization for XEH; useful when all objects in the mission are not XEH compatible. refs #15038

--- addons/extended_eventhandlers/CfgVehicles.hpp
+++ addons/extended_eventhandlers/CfgVehicles.hpp
@@ -483,4 +483,11 @@ class CfgVehicles {
 	class CruiseMissile2: MQ9PredatorB {
 		class EventHandlers { EXTENDED_EVENTHANDLERS };
 	};
+
+	// Custom object that can be used to force XEH initialization even if no XEH compatible object is on the Map.
+	class SLX_XEH_Logic: Logic {
+		displayName = "XEH (backup) Initialization Logic";
+		vehicleClass = "Modules";
+		class EventHandlers { EXTENDED_EVENTHANDLERS };
+	};
 };
