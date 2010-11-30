8e88dbd966926ad830e327980634cb588771afb5
Sickboy <sb@dev-heaven.net>
2010-11-30 18:27:29 +0100

    Backup

--- addons/common/config.cpp
+++ addons/common/config.cpp
@@ -20,12 +20,13 @@ class CfgPatches
 
 class CfgVehicles {
 	// Override - no need to double compile or compile functions at the same time (main disabled)
+	// Also use to kick-off XEH as final resort.
 	class Logic;
 	class FunctionsManager: Logic
 	{
 		class EventHandlers
 		{
-			init = "textLogFormat ['PRELOAD_ Functions\init %1', [_this, BIS_functions_mainscope]]; 	if (isnil 'BIS_functions_mainscope') then 	{ BIS_functions_mainscope = _this select 0;  if (isServer) then {_dummy = BIS_functions_mainscope spawn {_this setpos [1000,10,0];}};} else {_this spawn { textLogFormat ['PRELOAD_ Functions\init  ERROR: deleting redundant FM! %1', [_this, (_this select 0), BIS_functions_mainscope]]; _mygrp = group (_this select 0); deleteVehicle (_this select 0); deleteGroup _mygrp;};}; if (isnil 'RE') then {[] execVM '\ca\Modules\MP\data\scripts\MPframework.sqf'};	";
+			init = "if(isnil'SLX_XEH_objects')then{call compile preprocessFile'extended_eventhandlers\InitXEH.sqf'};_this call SLX_XEH_EH_Init; textLogFormat ['PRELOAD_ Functions\init %1', [_this, BIS_functions_mainscope]]; 	if (isnil 'BIS_functions_mainscope') then 	{ BIS_functions_mainscope = _this select 0;  if (isServer) then {_dummy = BIS_functions_mainscope spawn {_this setpos [1000,10,0];}};} else {_this spawn { textLogFormat ['PRELOAD_ Functions\init  ERROR: deleting redundant FM! %1', [_this, (_this select 0), BIS_functions_mainscope]]; _mygrp = group (_this select 0); deleteVehicle (_this select 0); deleteGroup _mygrp;};}; if (isnil 'RE') then {[] execVM '\ca\Modules\MP\data\scripts\MPframework.sqf'};	";
 		};
 	};
 };
