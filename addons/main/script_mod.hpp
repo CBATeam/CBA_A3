f82103a942e0288a95defbfd5667c43506b87d43
Sickboy <sb@dev-heaven.net>
2011-02-16 22:29:11 +0100

    bump

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,8 +3,8 @@
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 0
 #define MINOR 7
-#define PATCHLVL 3
-#define BUILD 147
+#define PATCHLVL 4
+#define BUILD 150
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
