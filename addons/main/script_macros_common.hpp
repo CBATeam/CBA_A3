de12a00ecd7cf236582835ab3989cf801f8d4aa3
Sickboy <sb@dev-heaven.net>
2011-08-19 15:17:50 +0200

    m

--- addons/main/script_macros_common.hpp
+++ addons/main/script_macros_common.hpp
@@ -975,7 +975,7 @@ Author:
 ------------------------------------------- */
 #define KEY_PARAM(KEY,NAME,DEF_VALUE) \
 	private #NAME; \
-	NAME = [toLower KEY, toUpper KEY, DEF_VALUE, RETNIL(_this)] call CBA_fnc_getArg;
+	NAME = [toLower KEY, toUpper KEY, DEF_VALUE, RETNIL(_this)] call CBA_fnc_getArg; \
 	TRACE_3("KEY_PARAM",KEY,NAME,DEF_VALUE)
 
 /* -------------------------------------------
