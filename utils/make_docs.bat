254ed9fe18e0266b50095e1bc9df90f7b62c00be
Sickboy <sb@dev-heaven.net>
2011-09-21 09:45:57 +0200

    Updated Docs

--- utils/make_docs.bat
+++ utils/make_docs.bat
@@ -2,7 +2,7 @@
 
 echo === Compiling documentation ===
 echo.
-perl "C:\Program Files (x86)\NaturalDocs-1.4\NaturalDocs" -i "..\addons" -o HTML "..\store\function_library" -p "ndocs-project" -s Default cba
+perl "C:\Program Files (x86)\NaturalDocs-1.52\NaturalDocs" -i "..\addons" -o HTML "..\store\function_library" -p "ndocs-project" -s Default cba
 
 echo.
 echo === Packaging documentation ===
