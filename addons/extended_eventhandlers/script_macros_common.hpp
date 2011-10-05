7e77b009ed36a6da5fd904c4eb63d64f5e79057b
Sickboy <sb@dev-heaven.net>
2011-10-05 20:48:24 +0200

    ~ ADDED: Local DISABLE_COMPILE_CACHE define to disable caching where COMPILE_FILE variants are involved (incl PREP, etc). refs #23841

--- addons/extended_eventhandlers/script_macros_common.hpp
+++ addons/extended_eventhandlers/script_macros_common.hpp
@@ -549,13 +549,16 @@ Author:
 
 // This only works for binarized configs after recompiling the pbos
 // TODO: Reduce amount of calls / code..
-#define COMPILE_FILE2_CFG_SYS(var1) (var1 call {_slx_xeh_compile = uiNamespace getVariable 'SLX_XEH_COMPILE'; if (isNil '_slx_xeh_compile') then { _this call compile preProcessFileLineNumbers 'extended_eventhandlers\init_compile.sqf' } else { _this call _slx_xeh_compile } })
 
-#ifdef DEBUG_MODE_FULL
-	#define COMPILE_FILE2_SYS(var1) (var1 call {_slx_xeh_compile = uiNamespace getVariable 'SLX_XEH_COMPILE'; if (isNil '_slx_xeh_compile') then { diag_log 'WARN: SLX_XEH_COMPILE function missing. compiling...'; _this call compile preProcessFileLineNumbers 'extended_eventhandlers\init_compile.sqf' } else { _this call _slx_xeh_compile } })
+// If DISABLE_COMPILE_CACHE is defined, use normal compile preProc...
+#ifdef DISABLE_COMPILE_CACHE
+	#define COMPILE_FILE2_CFG_SYS(var1) compile preProcessFileLineNumbers var1
+	#define COMPILE_FILE2_SYS(var1) COMPILE_FILE2_CFG_SYS(var1)
 #else
+	#define COMPILE_FILE2_CFG_SYS(var1) (var1 call {_slx_xeh_compile = uiNamespace getVariable 'SLX_XEH_COMPILE'; if (isNil '_slx_xeh_compile') then { _this call compile preProcessFileLineNumbers 'extended_eventhandlers\init_compile.sqf' } else { _this call _slx_xeh_compile } })
 	#define COMPILE_FILE2_SYS(var1) (var1 call SLX_XEH_COMPILE)
 #endif
+
 #define COMPILE_FILE_SYS(var1,var2,var3) COMPILE_FILE2_SYS('PATHTO_SYS(var1,var2,var3)')
 #define COMPILE_FILE_CFG_SYS(var1,var2,var3) COMPILE_FILE2_CFG_SYS('PATHTO_SYS(var1,var2,var3)')
 
