5afba6bd5b8d8dbe68c42dbe9837c657311a4bf5
Sickboy <sb@dev-heaven.net>
2011-10-10 19:55:45 +0200

    m

--- addons/extended_eventhandlers/script_macros_common.hpp
+++ addons/extended_eventhandlers/script_macros_common.hpp
@@ -280,31 +280,31 @@ Author:
 	Spooner
 ------------------------------------------- */
 #define PFORMAT_1(MESSAGE,A) \
-	format ['%1 A=%2', MESSAGE, RETNIL(A)]
+	format ['%1: A=%2', MESSAGE, RETNIL(A)]
 
 #define PFORMAT_2(MESSAGE,A,B) \
-	format ['%1 A=%2, B=%3', MESSAGE, RETNIL(A), RETNIL(B)]
+	format ['%1: A=%2, B=%3', MESSAGE, RETNIL(A), RETNIL(B)]
 
 #define PFORMAT_3(MESSAGE,A,B,C) \
-	format ['%1 A=%2, B=%3, C=%4', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C)]
+	format ['%1: A=%2, B=%3, C=%4', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C)]
 
 #define PFORMAT_4(MESSAGE,A,B,C,D) \
-	format ['%1 A=%2, B=%3, C=%4, D=%5', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D)]
+	format ['%1: A=%2, B=%3, C=%4, D=%5', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D)]
 
 #define PFORMAT_5(MESSAGE,A,B,C,D,E) \
-	format ['%1 A=%2, B=%3, C=%4, D=%5, E=%6', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E)]
+	format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E)]
 
 #define PFORMAT_6(MESSAGE,A,B,C,D,E,F) \
-	format ['%1 A=%2, B=%3, C=%4, D=%5, E=%6, F=%7', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F)]
+	format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6, F=%7', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F)]
 
 #define PFORMAT_7(MESSAGE,A,B,C,D,E,F,G) \
-	format ['%1 A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G)]
+	format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G)]
 
 #define PFORMAT_8(MESSAGE,A,B,C,D,E,F,G,H) \
-	format ['%1 A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8, H=%9', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G), RETNIL(H)]
+	format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8, H=%9', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G), RETNIL(H)]
 
 #define PFORMAT_9(MESSAGE,A,B,C,D,E,F,G,H,I) \
-	format ['%1 A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8, H=%9, I=%10', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G), RETNIL(H), RETNIL(I)]
+	format ['%1: A=%2, B=%3, C=%4, D=%5, E=%6, F=%7, G=%8, H=%9, I=%10', MESSAGE, RETNIL(A), RETNIL(B), RETNIL(C), RETNIL(D), RETNIL(E), RETNIL(F), RETNIL(G), RETNIL(H), RETNIL(I)]
 
 
 #ifdef DEBUG_MODE_FULL
