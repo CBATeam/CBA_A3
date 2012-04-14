c294e58bf573853ac32ad6cd2d14ad16ec3b011c
Sickboy <sb@dev-heaven.net>
2012-04-14 17:51:30 +0200

    Fixed Extended_EventHandlers backwards compatibility version information

--- addons/xeh/CfgPatches.hpp
+++ addons/xeh/CfgPatches.hpp
@@ -17,7 +17,9 @@ class CfgPatches {
 		worlds[] = {};
 		requiredAddons[] = {};
 		requiredVersion = REQUIRED_VERSION;
-		VERSION_CONFIG;
+		version = "4.0.0"; // Due to older mod versions requiring > 3,3,3 etc
+		versionStr = "4.0.0";
+		versionAr[] = {4,0,0};
 		author[] = {"CBA Team", "Solus", "Killswitch"};
 		authorUrl = "http://dev-heaven.net/projects/cca";
 	};
