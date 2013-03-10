066b522db9413994edcf2f6692b6a72d792708da
Patrick Roza <roza@sixnetworks.net>
2013-03-10 13:56:29 +0100

    A3 UiHelper

---
+++ addons/ui_helper/CfgRscStd.hpp
@@ -0,0 +1,16 @@
+class RscStandardDisplay;
+class RscListBox;
+class RscDisplayMultiplayer: RscStandardDisplay {
+	class ControlsBackground {
+		class CA_Cancel;
+		class CA_Join: CA_Cancel {
+			onMouseButtonDown = "uiNamespace setVariable ['CBA_isCached', nil]";
+			onKeyDown = "uiNamespace setVariable ['CBA_isCached', nil]";
+		};
+	};
+	class Controls {
+		class CA_ValueSessions: RscListBox {
+			onMouseButtonDown = "uiNamespace setVariable ['CBA_isCached', nil]";
+		};
+	};
+};
---
+++ addons/ui_helper/config.cpp
@@ -0,0 +1,14 @@
+#include "script_component.hpp"
+class CfgPatches {
+	class ADDON {
+		units[] = {};
+		weapons[] = {};
+		worlds[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {
+			"CBA_common"
+		};
+	};
+};
+
+#include "CfgRscStd.hpp"
