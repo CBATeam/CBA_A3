3059fdcc107a4e5a6f3a3db505d0b8dbd0141693
ViperMaul <ViperMaul@gmail.com>
2013-04-28 18:44:19 -0700

    ~Fixed RPT Warning: no type entry inside class, fixes #71830

--- addons/ui_helper/CfgRscStd.hpp
+++ addons/ui_helper/CfgRscStd.hpp
@@ -1,8 +1,10 @@
-class RscStandardDisplay;
 class RscListBox;
+class RscStandardDisplay;
+class RscButtonMenuCancel;
+
 class RscDisplayMultiplayer: RscStandardDisplay {
 	class ControlsBackground {
-		class CA_Cancel;
+		class CA_Cancel: RscButtonMenuCancel{};
 		class CA_Join: CA_Cancel {
 			onMouseButtonDown = "uiNamespace setVariable ['CBA_isCached', nil]";
 			onKeyDown = "uiNamespace setVariable ['CBA_isCached', nil]";
