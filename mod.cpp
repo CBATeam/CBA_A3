5a7a4732516fa8442d1d81726e6dbac1a997dc5d
ViperMaul <ViperMaul@gmail.com>
2013-06-24 21:57:17 -0700

    ~Fixed: Invalid picture shown in the Expansions Dialog,fixes #72132

---
+++ mod.cpp
@@ -0,0 +1,5 @@
+name = "Community Base Addons: A3 Beta";
+picture = "logo_cba_ca.paa";
+actionName = "Website";
+action = "http://dev-heaven.net/projects/cca";
+description = "Bugtracker: http://dev-heaven.net/projects/cca<br />Documentation: http://dev-heaven.net/projects/cca";
