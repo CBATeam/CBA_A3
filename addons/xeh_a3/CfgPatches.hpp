7006373978db789c1fd0bda2db501a8653d27b18
ViperMaul <ViperMaul@gmail.com>
2014-08-30 11:10:02 -0700

    update required addons cba_xeh_a3, fixes #75080

--- addons/xeh_a3/CfgPatches.hpp
+++ addons/xeh_a3/CfgPatches.hpp
@@ -5,7 +5,7 @@ class CfgPatches {
 		requiredVersion = REQUIRED_VERSION;
 		requiredAddons[] = {
 			"CBA_MAIN_A3", "CBA_XEH",
-			"CAData", "A3_Characters_F", "A3_Boat_F", "A3_animals_f", "A3_air_f", "A3_soft_f", "A3_static_f", "A3_weapons_f", "a3_structures_f" // "A3_armor_f"
+			"A3_Characters_F", "A3_Boat_F", "A3_animals_f", "A3_air_f", "A3_soft_f", "A3_static_f", "A3_weapons_f", "a3_structures_f" // "A3_armor_f"
 			//"CAData", // All, Car, Tank, Helicopter, Plane, Ship, ParachuteBase
 			//"CA_Missions_AmbientCombat", // AmbientCombatManager
 			//"CA_Modules_Functions", // FunctionsManager, PreloadManager
