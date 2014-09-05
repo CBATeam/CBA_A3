c2423e358acd5a3b5e0f895704e74cc0f16b1a5d
Killswitch <killswitch@zeus-community.net>
2014-09-05 21:14:18 +0200

    Removed redundant declarations. Keep classes specific to Arma 3 in xeh_a3

--- addons/xeh/CfgVehicles.hpp
+++ addons/xeh/CfgVehicles.hpp
@@ -2,33 +2,44 @@ class CfgVehicles {
 	class All {
 		class EventHandlers { EXTENDED_EVENTHANDLERS };
 	};
-	class Static: All {
+	class ArtilleryTarget: All {
 		XEH_DISABLED;
 	};
-	class Strategic;
-	class ReammoBox: Strategic {
-		XEH_ENABLED;
+	class FireSectorTarget: All {
+		XEH_DISABLED;
+	};
+	class LaserTarget: All  {
+		XEH_DISABLED;
+	};
+	class NVTarget: All  {
+		XEH_DISABLED;
+	};
+	class Static: All {
+		XEH_DISABLED;
 	};
 	class Thing: All {
 		XEH_DISABLED;
 	};
-	class LandVehicle;
-	class Car: LandVehicle {
-		class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
+
+	class Strategic;
+	class ReammoBox: Strategic {
+		XEH_ENABLED;
 	};
-	class Tank: LandVehicle {
-		class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
-	};
+
 	class Air;
 	class Helicopter: Air {
 		class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
 	};
+	class ParachuteBase: Helicopter {
+		XEH_DISABLED;
+	};
 	class Plane: Air {
 		class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
 	};
-	class AllVehicles;
-	class Ship: AllVehicles {
-		class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
+
+	class StaticWeapon;
+	class StaticCannon: StaticWeapon {
+		class Eventhandlers { EXTENDED_EVENTHANDLERS };
 	};
 
 	// Custom object that can be used to force XEH initialization even if no XEH compatible object is on the Map.
--- addons/xeh_a3/CfgVehicles.hpp
+++ addons/xeh_a3/CfgVehicles.hpp
@@ -1,109 +1,10 @@
 class CfgVehicles {
-        class All {
-                class EventHandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class ArtilleryTarget: All {
-                XEH_DISABLED;
-        };
-        class FireSectorTarget: All {
-                XEH_DISABLED;
-        };
-        class LaserTarget: All  {
-                XEH_DISABLED;
-        };
-        class NVTarget: All  {
-                XEH_DISABLED;
-        };
-        class Air;
-        class Helicopter: Air {
-                class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class ParachuteBase: Helicopter {
-                XEH_DISABLED;
-        };
-        class Plane: Air {
-                class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
-        };
         class PlaneWreck;
         class Plane_Fighter_03_wreck_F: PlaneWreck
         {
                 XEH_DISABLED;
         };
-        class StaticWeapon;
-        class StaticCannon: StaticWeapon {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
 
-        // No need to do for logics
-        /*
-        class Logic;
-        class Module_F: Logic {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class BIS_Effect_FilmGrain: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class BIS_Effect_Day: BIS_Effect_FilmGrain {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class BIS_Effect_MovieNight: BIS_Effect_FilmGrain {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class BIS_Effect_Sepia: BIS_Effect_FilmGrain {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class Site_Military_Base: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class Site_FT_Base: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class Site_Ambient: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class Site_Minefield: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class Site_Patrol: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportRequester: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_Artillery: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_Virtual_Artillery: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_CAS_Heli: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_Virtual_CAS_Heli: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_CAS_Bombing: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_Virtual_CAS_Bombing: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_Drop: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_Virtual_Drop: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_Transport: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class SupportProvider_Virtual_Transport: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        class UAVCommand: Module_F {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
-        };
-        */
         class Land_FirePlace_F;
         class FirePlace_burning_F: Land_FirePlace_F {
                 class EventHandlers { EXTENDED_EVENTHANDLERS };
@@ -126,28 +27,30 @@ class CfgVehicles {
         };
         class Thing;
         class test_EmptyObjectForBubbles: Thing {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers { EXTENDED_EVENTHANDLERS };
         };
         class test_EmptyObjectForFireBig: test_EmptyObjectForBubbles {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers { EXTENDED_EVENTHANDLERS };
         };
         class test_EmptyObjectForSmoke: test_EmptyObjectForBubbles {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers { EXTENDED_EVENTHANDLERS };
         };
         class WeaponHolder;
         class B_AssaultPack_khk_holder: WeaponHolder {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers { EXTENDED_EVENTHANDLERS };
         };
         class StaticMortar;
         class Mortar_01_Base_F: StaticMortar {
-                class Eventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers { EXTENDED_EVENTHANDLERS };
         };
+        class Air;
+        class Helicopter: Air { class EventHandlers; };
         class Helicopter_Base_F: Helicopter {
-                class Eventhandlers: Eventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers: EventHandlers { EXTENDED_EVENTHANDLERS };
         };
         class Ship;
         class Ship_F: Ship {
-                class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers: DefaultEventHandlers { EXTENDED_EVENTHANDLERS };
         };
         class B_Soldier_base_F;
         class B_Soldier_F: B_Soldier_base_F { class EventHandlers; };
@@ -156,13 +59,13 @@ class CfgVehicles {
         };
         class Car;
         class Car_F: Car {
-                class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers: DefaultEventHandlers { EXTENDED_EVENTHANDLERS };
         };
         class Quadbike_Base_F: Car_F {
-                class EventHandlers: Eventhandlers { EXTENDED_EVENTHANDLERS };
+                class EventHandlers: EventHandlers { EXTENDED_EVENTHANDLERS };
         };
         class Tank;
         class Tank_F: Tank {
-            class Eventhandlers: DefaultEventhandlers { EXTENDED_EVENTHANDLERS };
+            class EventHandlers: DefaultEventHandlers { EXTENDED_EVENTHANDLERS };
         };
 };
