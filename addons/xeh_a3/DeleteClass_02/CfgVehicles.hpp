382b5ea15f9fefb3d32f997563a704c567b6053b
ViperMaul <ViperMaul@gmail.com>
2014-10-13 01:01:35 -0700

    Avoids hang, but with caveats, fixes #75132, refs #71906

--- addons/xeh_a3/DeleteClass_02/CfgVehicles.hpp
+++ addons/xeh_a3/DeleteClass_02/CfgVehicles.hpp
@@ -6,22 +6,23 @@ class CfgVehicles {
 	class Ship_F;
 	class Truck_F;
 
+
 	class C_man_1: Civilian_F {
-		delete EventHandlers; // Eventhandlers
+		delete Eventhandlers; // Eventhandlers
 	};
 	class Helicopter_Base_F: Helicopter {
-		delete EventHandlers; // Eventhandlers
+		delete Eventhandlers; // Eventhandlers
 	};
 	class Boat_Civil_01_base_F: Ship_F {
-		delete EventHandlers; // Eventhandlers
+		delete Eventhandlers; // Eventhandlers
 	};
 	class Offroad_01_repair_base_F: Offroad_01_base_F {
-		delete EventHandlers; // Eventhandlers
+		delete Eventhandlers; // Eventhandlers
 	};
 	class Quadbike_01_base_F: Car_F {
-		delete EventHandlers; // Eventhandlers
+		delete Eventhandlers; // Eventhandlers
 	};
 	class Van_01_base_F: Truck_F {
-		delete EventHandlers; // Eventhandlers
+		delete Eventhandlers; // Eventhandlers
 	};
 };
