a9b37fdbd2885fd41eadf9fbcdaf084d0ee4d8f4
ViperMaul <ViperMaul@gmail.com>
2015-01-21 21:39:07 -0800

    Version bump

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,7 +4,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define PATCHLVL 10
-#define BUILD 150117
+#define BUILD 150121
 //We will use the DATE for the BUILD# in the format YYMMDD - VM
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
@@ -12,7 +12,7 @@
 
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.26
+#define REQUIRED_VERSION 1.36
 
 /*
 // Defined DEBUG_MODE_NORMAL in a few CBA_fncs to prevent looped logging :)
