de3a1ea387f838676768e0b5cc34c2cb50fac524
Nou <cliff.foster@idi-systems.com>
2015-01-23 11:18:13 -0800

    Empty CfgPatches entry caused error on mission load.

--- addons/extended_eventhandlers/config.cpp
+++ addons/extended_eventhandlers/config.cpp
@@ -1,4 +1,15 @@
 class cfgPatches
 {
-    class cba_EE{}; // no paramaters
+    class cba_EE {
+        units[] = {};
+		weapons[] = {};
+		worlds[] = {};
+		requiredAddons[] = {};
+		requiredVersion = 0.1;
+		version = "4.0.0"; // Due to older mod versions requiring > 3,3,3 etc
+		versionStr = "4.0.0";
+		versionAr[] = {4,0,0};
+		author[] = {"CBA Team", "Solus", "Killswitch"};
+		authorUrl = "http://dev-heaven.net/projects/cca";
+    };
 };
