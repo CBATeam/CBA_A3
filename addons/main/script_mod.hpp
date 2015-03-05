c2761a8a59308c14fca6a572962bbc17fa83f62a
ViperMaul <vipermaul@gmail.com>
2015-03-04 16:21:19 -0800

    version bump for v1.40

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,7 +4,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define PATCHLVL 20
-#define BUILD 150126
+#define BUILD 150304
 //We will use the DATE for the BUILD# in the format YYMMDD - VM
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
