dc0aee28624af8b450e81b46eff6716e41eb87ff
ViperMaul <vipermaul@gmail.com>
2015-05-30 08:13:58 -0700

    v bump

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,8 +3,8 @@
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 1
 #define MINOR 1
-#define PATCHLVL 21
-#define BUILD 150527
+#define PATCHLVL 22
+#define BUILD 150530
 //We will use the DATE for the BUILD# in the format YYMMDD - VM
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
