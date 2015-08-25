ec86f5460331a11a7fda4234ba5097acfb29f177
Killswitch <killswitch@zeus-community.net>
2015-08-25 20:03:48 +0200

    Move NaturalDocs overview and logo file out of addons, since they conflict with the make.py build scripts

--- tools/make_docs.bat
+++ tools/make_docs.bat
@@ -2,12 +2,16 @@
 
 echo === Compiling documentation ===
 echo.
-perl "C:\Program Files (x86)\NaturalDocs-1.52\NaturalDocs" -i "..\addons" -o HTML "..\docs" -p "ndocs-project" -s Default cba
+copy overview.txt ..\addons
+copy CBA_logo_large.png ..\addons
+perl "C:\Program Files (x86)\NaturalDocs-1.52\NaturalDocs" -r -i "..\addons" -o HTML "..\docs" -p "ndocs-project" -s Default cba
+del /Q ..\addons\overview.txt ..\addons\CBA_logo_large.png 
 
 echo.
 echo === Packaging documentation ===
 echo.
 del /F /Q "..\store\function_library.tar"
+del /Q ..\store\functions_library.tar
 tar -C ..\ -cf ..\store\function_library.tar docs
 
 pause
