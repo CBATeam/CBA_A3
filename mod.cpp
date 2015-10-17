b53b6eca5340b16502b33020612553662457521a
ViperMaul <vipermaul@gmail.com>
2015-10-16 20:46:38 -0700

    bump

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
-name = "Community Base Addons v2.1.1";
+name = "Community Base Addons v2.1.2";
 picture = "logo_cba_ca.paa";
 actionName = "Website";
 action = "http://dev.withsix.com/projects/cba-a3";
