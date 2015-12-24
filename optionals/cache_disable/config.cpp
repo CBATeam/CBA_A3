531a132e8c46456bb1ba0a4238a61591ec411e27
commy2 <commy-2@gmx.de>
2015-12-24 13:16:04 +0100

    remove obsolete auto enable xeh pbo

--- optionals/cache_disable/config.cpp
+++ optionals/cache_disable/config.cpp
@@ -1,28 +1,23 @@
 #include "script_component.hpp"
 
-class CfgPatches
-{
-	class ADDON
-	{
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = REQUIRED_VERSION;
-		requiredAddons[] = {"Extended_EventHandlers", "CBA_Main"};
-		version = VERSION;
-		author[] = {"Sickboy"};
-		authorUrl = "http://dev-heaven.net/projects/cca";
-	};
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"CBA_Extended_EventHandlers", "CBA_Main"};
+        version = VERSION;
+        author[] = {"Sickboy"};
+        authorUrl = "http://dev-heaven.net/projects/cca";
+    };
 };
 
-class CfgSettings
-{
-	class CBA
-	{
-		class Caching
-		{
-			xeh = 0;
-			compile = 0;
-			functions = 0;
-		};
-	};
+class CfgSettings {
+    class CBA {
+        class Caching {
+            xeh = 0;
+            compile = 0;
+            functions = 0;
+        };
+    };
 };
--- optionals/enable_auto_xeh/script_component.hpp
+++
@@ -1,7 +0,0 @@
-#define COMPONENT enable_auto_xeh
-#include "\x\cba\addons\main\script_mod.hpp"
-
-#undef REQUIRED_VERSION
-#define REQUIRED_VERSION 1.00
-
-#include "\x\cba\addons\main\script_macros.hpp"
