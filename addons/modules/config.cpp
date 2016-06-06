02fb205d3b2b8f95363fcb16f1d5b73b1bb88d63
Tom Yost <wiredtiger.ty@gmail.com>
2016-06-06 17:27:46 -0400

    Update CfgPatches and Stringtable

--- addons/modules/config.cpp
+++ addons/modules/config.cpp
@@ -2,12 +2,15 @@
 
 class CfgPatches {
     class ADDON {
-        units[] = {"CBA_ModuleAttack","CBA_ModuleDefend","CBA_ModulePatrol"};
-        requiredVersion = 0.1;
-        requiredAddons[] = {"A3_Modules_F"};
         author = "$STR_CBA_Author";
+        name = CSTRING(component);
+        url = "$STR_CBA_URL";
+        units[] = {"CBA_ModuleAttack","CBA_ModuleDefend","CBA_ModulePatrol"};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3_Modules_F","CBA_common"};
+        version = VERSION;
         authors[] = {"WiredTiger"};
-        authorUrl = "";
+        
     };
 };
 
