50d17ec0e51d79834fec8c54ff7ce873d94b82f0
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2016-06-28 19:18:11 +0200

    Added Linux compatibility

--- addons/statemachine/CfgEventHandlers.hpp
+++ addons/statemachine/CfgEventHandlers.hpp
@@ -1,11 +1,11 @@
 class Extended_PreStart_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_preStart));
+        init = QUOTE(if (!IS_LINUX) then {call COMPILE_FILE(XEH_preStart)});
     };
 };
 
 class Extended_PreInit_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_preInit));
+        init = QUOTE(if (!IS_LINUX) then {call COMPILE_FILE(XEH_preInit)});
     };
 };
