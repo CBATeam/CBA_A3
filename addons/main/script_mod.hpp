606f6e2d7b13880cdba0b74a3742da2295bf4b1b
commy2 <commy-2@gmx.de>
2016-07-13 13:16:05 +0200

    bump required version to 1.62

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -12,7 +12,7 @@
 
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.54
+#define REQUIRED_VERSION 1.62
 
 /*
 // Defined DEBUG_MODE_NORMAL in a few CBA_fncs to prevent looped logging :)
