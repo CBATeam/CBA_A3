2bb08b90579ac9e686ede86b7e7e2550ed0256a9
commy2 <commy-2@gmx.de>
2016-08-22 11:12:46 +0200

    tweak ERROR

--- addons/diagnostic/gui.hpp
+++ addons/diagnostic/gui.hpp
@@ -4,7 +4,7 @@ class RscStructuredText;
 class RscTitles {
     class GVAR(Error) {
         idd = -1;
-        duration = 5;
+        duration = 10;
         fadeIn = 0;
         fadeOut = 0.5;
         movingEnable = 0;
@@ -16,10 +16,10 @@ class RscTitles {
                 font = "RobotoCondensedBold";
                 sizeEx = 0.55 * GUI_GRID_CENTER_H;
                 x =  0 * GUI_GRID_CENTER_W + GUI_GRID_CENTER_X;
-                y =  0 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
+                y =  5 * GUI_GRID_CENTER_H + GUI_GRID_CENTER_Y;
                 w = 40 * GUI_GRID_CENTER_W;
                 h = 10 * GUI_GRID_CENTER_H;
-                colorBackground[] = {1,0.4,0,0.8};
+                colorBackground[] = {1,0.2,0,0.8};
             };
         };
     };
