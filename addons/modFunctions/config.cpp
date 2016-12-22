3bfdd933a6340e04f77d31af92f4e873b4734615
Vincent <agpheaddikilla@yahoo.de>
2016-12-22 21:01:53 +0100

    Update config.cpp

--- addons/modFunctions/config.cpp
+++ addons/modFunctions/config.cpp
@@ -1,4 +1,8 @@
 
+class CfgFunctions {
+    #include "CfgFunctions.cpp"
+};
+
 class Extended_PreStart_EventHandlers {
 	class cba_modFunctions {
 		init = "call compile preProcessFileLineNumbers '\x\cba\addons\modFunctions\XEH_preStart.sqf'";
