78dc827b6c4a724eacf521c0b13aae99eee6c9e7
Vincent <agpheaddikilla@yahoo.de>
2016-12-22 21:04:55 +0100

    Create CfgPatches.cpp

---
+++ addons/modFunctions/CfgPatches.cpp
@@ -0,0 +1,14 @@
+
+#include "script_component.hpp"
+
+class cba_modFunctions {
+    author = "Vincent Heins";
+    name = CSTRING(component);
+    url = "$STR_CBA_URL";
+    units[] = {};
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {"CBA_common"};
+    version = VERSION;
+    authors[] = {"TheMysteriousVincent/AGPHeaddikilla/Vincent Heins"};
+};
+    
