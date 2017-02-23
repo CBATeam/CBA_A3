3c2b703dadc114bc2725793812a590743338b00b
fishykins <caspar.m.green@gmail.com>
2017-02-23 08:50:20 +0000

    Update CfgFunctions.hpp

--- addons/music/CfgFunctions.hpp
+++ addons/music/CfgFunctions.hpp
@@ -10,37 +10,37 @@ class CfgFunctions
                 description = "Get the config path of given music class";
                 file = "\x\cba\addons\music\fnc_getMusicPath.sqf";
             };
-		      	// FSH_fnc_getMusicData
+            // FSH_fnc_getMusicData
             class getMusicData
             {
                 description = "Get data attached to music class";
                 file = "\x\cba\addons\music\fnc_getMusicData.sqf";
             };
-		      	// FSH_fnc_searchMusic
+            // FSH_fnc_searchMusic
             class searchMusic
             {
                 description = "Find music of spesified type";
                 file = "\x\cba\addons\music\fnc_searchMusic.sqf";
             };
-	      		// FSH_fnc_compileMusic
+            // FSH_fnc_compileMusic
             class compileMusic
             {
                 description = "Find all music in game";
-               file = "\x\cba\addons\music\fnc_compileMusic.sqf";
+                file = "\x\cba\addons\music\fnc_compileMusic.sqf";
             };
-	      		// FSH_fnc_getMusicPlaying
+            // FSH_fnc_getMusicPlaying
             class getMusicPlaying
             {
                 description = "returns the data on currently playing music";
                 file = "\x\cba\addons\music\fnc_getMusicPlaying.sqf";
             };
-			      // FSH_fnc_playMusic
+              // FSH_fnc_playMusic
             class playMusic
             {
                 description = "Duh";
                 file = "\x\cba\addons\music\fnc_playMusic.sqf";
             };
-		      	// FSH_fnc_stopMusic
+            // FSH_fnc_stopMusic
             class stopMusic
             {
                 description = "Duh";
