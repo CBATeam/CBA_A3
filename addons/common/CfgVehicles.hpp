91151d8a526caa10935ea5ce7e450a7e84bca6e7
commy2 <commy-2@gmx.de>
2017-03-17 21:05:01 +0100

    remove obsolete eventhandler

--- addons/common/CfgVehicles.hpp
+++ addons/common/CfgVehicles.hpp
@@ -4,9 +4,5 @@ class CfgVehicles {
     class CBA_NamespaceDummy: Static {
         scope = 1;
         displayName = "";
-
-        class EventHandlers {
-            init = "(_this select 0) enableSimulation false";
-        };
     };
 };
