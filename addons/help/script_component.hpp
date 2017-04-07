aef01045022ecf1ae702b8907d1bb8d55ef42ddb
commy2 <commy-2@gmx.de>
2017-04-07 21:05:57 +0200

    fix and simplify the help component ui

--- addons/help/script_component.hpp
+++ addons/help/script_component.hpp
@@ -11,8 +11,22 @@
 
 #include "\x\cba\addons\main\script_macros.hpp"
 
-#include "script_dialog_defines.hpp"
+#include "\a3\ui_f\hpp\defineCommonGrids.inc"
+#include "\a3\ui_f\hpp\defineDIKCodes.inc"
+#include "\a3\ui_f\hpp\defineResincl.inc"
 
-#define CBA_CREDITS_CONT_IDC 2222710
-#define CBA_CREDITS_VER_IDC 2222712
-#define CBA_CREDITS_VER_BTN_IDC 2222713
+#define POS_X(N) ((N) * GUI_GRID_W + GUI_GRID_X)
+#define POS_Y(N) ((N) * GUI_GRID_H + GUI_GRID_Y)
+#define POS_W(N) ((N) * GUI_GRID_W)
+#define POS_H(N) ((N) * GUI_GRID_H)
+
+#define POS_X_CENTERED(N) ((N) * GUI_GRID_W + GUI_GRID_CENTER_X)
+#define POS_X_RIGHT(N) (safezoneW - 1 * (N) * GUI_GRID_W + GUI_GRID_X)
+
+#define POS_X_MAIN_MENU(N) safezoneX
+#define POS_Y_MAIN_MENU(N) (safezoneY + safezoneH - 1 * (N) * (pixelH * pixelGrid * 2))
+#define POS_W_MAIN_MENU(N) safezoneW
+#define POS_H_MAIN_MENU(N) ((N) * (pixelH * pixelGrid * 2))
+
+#define IDC_VERSION_TEXT 222712
+#define IDC_VERSION_BUTTON 222713
