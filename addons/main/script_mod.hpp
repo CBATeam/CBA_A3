64c91ceff75203fbc09d6639fa3ec45b5bd5f6d1
ViperMaul <vipermaul@gmail.com>
2017-04-08 12:45:51 -0500

    Prep for v3.3.0

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -2,9 +2,9 @@
 #define PREFIX cba
 // TODO: Consider Mod-wide or Component-narrow versions  (or both, depending on wishes!)
 #define MAJOR 3
-#define MINOR 2
-#define PATCHLVL 1
-#define BUILD 170227
+#define MINOR 3
+#define PATCHLVL 0
+#define BUILD 170408
 //We will use the DATE for the BUILD# in the format YYMMDD - VM
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
