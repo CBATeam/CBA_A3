c5a83daf3d89ac6e745201c8a76deeb385376b1f
commy2 <commy-2@gmx.de>
2017-04-11 15:04:32 +0200

    move to ui component

--- addons/keybinding/script_component.hpp
+++ addons/keybinding/script_component.hpp
@@ -1,5 +1,10 @@
 #define COMPONENT keybinding
 #include "\x\cba\addons\main\script_mod.hpp"
+#include "\x\cba\addons\main\script_macros.hpp"
+
+#include "\a3\ui_f\hpp\defineDIKCodes.inc"
+
+//#define DEBUG_ENABLED_KEYBINDING
 
 #ifdef DEBUG_ENABLED_KEYBINDING
     #define DEBUG_MODE_FULL
@@ -8,11 +13,6 @@
 #ifdef DEBUG_SETTINGS_KEYBINDING
     #define DEBUG_SETTINGS DEBUG_SETTINGS_KEYBINDING
 #endif
-
-#include "\x\cba\addons\main\script_macros.hpp"
-
-#include "\a3\ui_f\hpp\defineCommonGrids.inc"
-#include "\a3\ui_f\hpp\defineDIKCodes.inc"
 
 #define PATHTO_SUB(var1,var2,var3,var4) MAINPREFIX\##var1\SUBPREFIX\##var2\##var3\##var4.sqf
 #define COMPILE_FILE_SUB(var1,var2,var3,var4) COMPILE_FILE2(PATHTO_SUB(var1,var2,var3,var4))
