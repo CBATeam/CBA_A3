24e23d614a94581955008b20f8dee4979fa0bb69
ViperMaul <vipermaul@gmail.com>
2017-06-27 10:59:53 -0500

    Prep for final release

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,7 +4,7 @@
 #define MAJOR 3
 #define MINOR 4
 #define PATCHLVL 0
-#define BUILD 170624
+#define BUILD 170627
 //We will use the DATE for the BUILD# in the format YYMMDD - VM
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
