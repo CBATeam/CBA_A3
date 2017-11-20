748d67b895661df388751c6f45f49672eb044b2d
commy2 <commy-2@gmx.de>
2017-11-20 13:01:36 +0100

    versioning, some changes from broken pr

--- addons/versioning/CfgEventHandlers.hpp
+++ addons/versioning/CfgEventHandlers.hpp
@@ -1,4 +1,3 @@
-
 class Extended_PreStart_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_FILE(XEH_preStart));
@@ -15,6 +14,6 @@ class Extended_PostInit_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_FILE(XEH_postInit));
         serverInit = QUOTE(call COMPILE_FILE(XEH_postInitServer));
-        clientInit = QUOTE(if !(isServer) then { call COMPILE_FILE(XEH_postInitClient) });
+        clientInit = QUOTE(if !(isServer) then {call COMPILE_FILE(XEH_postInitClient)});
     };
 };
