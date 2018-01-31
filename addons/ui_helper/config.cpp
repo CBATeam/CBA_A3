4b3e11dce495aae308626553353ab874c88b0307
ViperMaul <vipermaul@gmail.com>
2018-01-31 10:23:44 -0600

    Temporarily re-adding deprecated script_dikCodes.hpp, refs #882

---
+++ addons/ui_helper/config.cpp
@@ -0,0 +1,16 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        author = "$STR_CBA_Author";
+        name = ECSTRING(ui,component);
+        url = "$STR_CBA_URL";
+        units[] = {};
+        weapons[] = {};
+        worlds[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"CBA_common", "A3_UI_F"};
+    };
+};
+
+#include "CfgRscStd.hpp"
