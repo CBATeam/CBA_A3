b062cd5b8e38dd0fc272f1f740b0455d20dff857
jonpas <jonpas33@gmail.com>
2018-04-30 19:26:48 +0200

    Bump to 3.7.0 RC2

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,7 +4,7 @@
 #define MAJOR 3
 #define MINOR 7
 #define PATCHLVL 0
-#define BUILD 180424
+#define BUILD 180430
 //We will use the DATE for the BUILD# in the format YYMMDD - VM
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
