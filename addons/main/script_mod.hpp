52b821f0769f554ff395daf8091604a1ff7f22a2
ViperMaul <ViperMaul@gmail.com>
2018-06-04 21:16:09 -0500

    Bump to 3.7.1.180604

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,7 +4,7 @@
 #define MAJOR 3
 #define MINOR 7
 #define PATCHLVL 1
-#define BUILD 180517
+#define BUILD 180604
 //We will use the DATE for the BUILD# in the format YYMMDD - VM
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
