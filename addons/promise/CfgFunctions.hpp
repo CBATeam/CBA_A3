af5b472679e0c2ccbb07af1a3ec2d29f86988d31
Marco Silipo <ma64765@w-hs.de>
2018-06-27 12:03:33 +0200

    Added promise system

---
+++ addons/promise/CfgFunctions.hpp
@@ -0,0 +1,11 @@
+class CfgFunctions {
+    class CBA {
+        class Network {
+            PATHTO_FNC(promise_create);
+            PATHTO_FNC(promise_callback);
+            PATHTO_FNC(promise_done);
+            PATHTO_FNC(promise_receiver);
+            PATHTO_FNC(promise_init);
+        };
+    };
+};
