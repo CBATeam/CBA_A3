7dea95f2ae4c3ef709a22ba1c507ad30d751fd96
robalo <rs@bzbit.com>
2018-09-19 15:05:08 +0300

    Split/extend the magwell classes by size

---
+++ addons/jam/magwells_762x54.hpp
@@ -0,0 +1,12 @@
+    class CBA_762x54R_SVD {
+        BI_mags[] = {
+            "10Rnd_762x54_Mag"
+        };
+    };
+
+    class CBA_762x54R_LINKS {
+        BI_belts[] = {
+            "150Rnd_762x54_Box",
+            "150Rnd_762x54_Box_Tracer"
+        };
+    };
---
+++ addons/jam/magwells_9x19.hpp
@@ -0,0 +1,5 @@
+    class CBA_9x19_MP5 {};          // H&K MP5
+    class CBA_9x19_STEYR {};        // AUG SMG, MPi, TMP
+    class CBA_9x19_P226 {};         // SIG P226
+    class CBA_9x19_P228 {};         // SIG P228
+    class CBA_9x19_P239 {};         // SIG P239
