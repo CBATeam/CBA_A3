1bc3d066e1211096bf4ac0ac84f393b1d89119b6
Drofseh <Drofseh@users.noreply.github.com>
2018-09-19 11:59:18 -0700

    add magwells for current IFA3, FOW, LEN weapons
    - Add magwells for current IFA3, FOW, LEN weapons
    - Also add magwells for non-magazine fed shotguns.
    All shotguns should use every magwell that is <= its tube capacity.

---
+++ addons/jam/magwells_45ACP.hpp
@@ -0,0 +1,7 @@
+    class CBA_45ACP_1911 {};        // Colt M1911
+    class CBA_45ACP_C96 {};         // Mauser C-96 in .45 ACP
+    class CBA_45ACP_Delisle {};     // De Lisle Carbine
+    class CBA_45ACP_Grease {};      // Grease Gun
+    class CBA_45ACP_Thompson_Stick {};  // Thompson stick magazines
+    class CBA_45ACP_Thompson_Drum {};   // Thompson drum magazines
+    class CBA_45ACP_Reising {};     // M50/M55 Reising
--- addons/jam/magwells_9x19.hpp
+++ addons/jam/magwells_9x19.hpp
@@ -3,3 +3,11 @@
     class CBA_9x19_P226 {};         // SIG P226
     class CBA_9x19_P228 {};         // SIG P228
     class CBA_9x19_P239 {};         // SIG P239
+    class CBA_9x19_MP40 {};         // MP40, MP38
+    class CBA_9x19_STEN {};         // STEN
+    class CBA_9x19_C96 {};          // Mauser C-96 in 9x19mm
+    class CBA_9x19_HiPower {};      // Browning HiPower
+    class CBA_9x19_P08 {};          // Luger P08
+    class CBA_9x19_P38 {};          // Walther P38
+    class CBA_9x19_Vis {};          // wz. 35 Vis (Radom)
+    class CBA_9x19_Welrod {};       // Welrod pistol in 9x19mm
