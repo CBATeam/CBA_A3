0a6c08f6c8e5dd218cc54b77e5055fe6594490b2
Drofseh <Drofseh@users.noreply.github.com>
2018-10-04 12:30:29 -0700

    JAM STANAG and 7.62.x51 (#997)
    * adjust STANAG mags, add large 7.62x51 mags
    
    * un-deprecate the STANAG 2D XL classes
    
    * minor fix
    
    - re-add STANAG 2D XL to magazineWell[] for 5.56 rifles
    - put correct comment for 5.56 FAMAS mag
    
    * fix missing ;
    
    * fix comment indentation
    
    * Remove XL twin drums from bullpups

--- addons/jam/magwells_68SPC.hpp
+++ addons/jam/magwells_68SPC.hpp
@@ -1,6 +1,8 @@
-    class CBA_68SPC_STANAG {};          // 6.8 SPC in a normal STANAG mag
-    class CBA_68SPC_STANAG_2D {};       // 6.8 SPC in a twin-drum mag
-    class CBA_68SPC_STANAG_2D_XL {};    // 6.8 SPC in a XL twin-drum mag
-    class CBA_68SPC_STANAG_L {};        // 6.8 SPC in a long STANAG mag
-    class CBA_68SPC_STANAG_XL {};       // 6.8 SPC in a drum or XL stick mag
-    class CBA_68x43_ACR {};             // 6.8mm Remington SPC
+    class CBA_68SPC_STANAG {};          // 6.8 SPC in a normal length STANAG mag, including small drums
+    class CBA_68SPC_STANAG_L {};        // 6.8 SPC in a long STANAG stick or coffin
+    class CBA_68SPC_STANAG_XL {};       // 6.8 SPC in an extra long STANAG stick or coffin mag
+    class CBA_68SPC_STANAG_2D {};       // 6.8 SPC in a twin-drum STANAG mag
+    class CBA_68SPC_STANAG_2D_XL {};    // 6.8 SPC in an extra large twin-drum STANAG mag
+
+    //Deprecated classes do not use
+    class CBA_68x43_ACR {};             // DEPRECATED, use CBA_68SPC_STANAG
