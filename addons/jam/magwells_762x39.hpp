fa47c43fdd31f16fd12c400b58ce7597a26a23eb
Drofseh <Drofseh@users.noreply.github.com>
2018-10-04 09:55:36 -0700

    fix comment indentation

--- addons/jam/magwells_762x39.hpp
+++ addons/jam/magwells_762x39.hpp
@@ -13,4 +13,4 @@
     class CBA_762x39_STANAG_L {};       // 762x39mm in a long STANAG stick or coffin
     class CBA_762x39_STANAG_XL {};      // 762x39mm in an extra long STANAG stick or coffin mag
     class CBA_762x39_STANAG_2D {};      // 762x39mm in a twin-drum STANAG mag
-    class CBA_762x39_STANAG_2D_XL {};    // 762x39mm in an extra large twin-drum STANAG mag
+    class CBA_762x39_STANAG_2D_XL {};   // 762x39mm in an extra large twin-drum STANAG mag
