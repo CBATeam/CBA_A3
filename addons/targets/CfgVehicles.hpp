a4ee257ebea1529f4f2e04205aed2d771ca14388
Tinter <a-348@hotmail.com>
2018-10-19 03:16:44 +0200

    Proper fix for hitpoints.

--- addons/targets/CfgVehicles.hpp
+++ addons/targets/CfgVehicles.hpp
@@ -1,24 +1,65 @@
 class CfgVehicles 
 {
-  class B_TargetSoldier;
-  class O_TargetSoldier;
-  class I_TargetSoldier;
+  class StaticWeapon;
+  class TargetSoldierBase: StaticWeapon
+  {
+    class Turrets;
+  };
+  class B_TargetSoldier: TargetSoldierBase
+  {
+    class Turrets: Turrets
+    {
+      class MainTurret;
+    };
+  };
+  class O_TargetSoldier: TargetSoldierBase
+  {
+    class Turrets: Turrets
+    {
+      class MainTurret;
+    };
+  };
+  class I_TargetSoldier: TargetSoldierBase
+  {
+    class Turrets: Turrets
+    {
+      class MainTurret;
+    };
+  };
   class B_TargetSoldier_CBA: B_TargetSoldier 
   {
-    class Turrets {};
     model = QPATHTOF(invisible_target.p3d);
     scope = 2;
+    class Turrets: Turrets
+    {
+      class MainTurret: MainTurret
+      {
+        class HitPoints{};
+      };
+    };
   };
   class O_TargetSoldier_CBA: O_TargetSoldier 
   {
-    class Turrets {};
     model = QPATHTOF(invisible_target.p3d);
     scope = 2;
+    class Turrets: Turrets
+    {
+      class MainTurret: MainTurret
+      {
+        class HitPoints{};
+      };
+    };
   };
   class I_TargetSoldier_CBA: I_TargetSoldier 
   {
-    class Turrets {};
     model = QPATHTOF(invisible_target.p3d);
     scope = 2;
+    class Turrets: Turrets
+    {
+      class MainTurret: MainTurret
+      {
+        class HitPoints{};
+      };
+    };
   };
 };
