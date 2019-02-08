7f0973b35ef2e961bbae0dfa3f37a0f110bb739b
Dedmen Miller <dedmen@dedmen.de>
2019-02-08 14:30:02 +0100

    Move flexiMenu functions to CfgFunctions

--- addons/ui/CfgFunctions.hpp
+++ addons/ui/CfgFunctions.hpp
@@ -17,6 +17,36 @@ class CfgFunctions {
                 description = "Open a previously defined menu by passing the exact parameters used with cba_fnc_fleximenu_add. Result: TBA (WIP)";
                 file = QUOTE(PATHTOF(flexiMenu\fnc_openMenuByDef.sqf));
             };
+            class flexiMenu_keyDown {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_keyDown.sqf));
+            };
+            class flexiMenu_keyUp {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_keyUp.sqf));
+            };
+            class flexiMenu_menu {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_menu.sqf));
+            };
+            class flexiMenu_list {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_list.sqf));
+            };
+            class flexiMenu_getMenuDef {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_getMenuDef.sqf));
+            };
+            class flexiMenu_getMenuOption {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_getMenuOption.sqf));
+            };
+            class flexiMenu_menuShortcut {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_menuShortcut.sqf));
+            };
+            class flexiMenu_mouseButtonDown {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_mouseButtonDown.sqf));
+            };
+            class flexiMenu_highlightCaretKey {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_highlightCaretKey.sqf));
+            };
+            class flexiMenu_execute {
+                file = QUOTE(PATHTOF(flexiMenu\fnc_execute.sqf));
+            };
             PATHTO_FNC(addPauseMenuOption);
             PATHTO_FNC(progressBar);
             PATHTO_FNC(getFov);
