55170f6e9e7a4ec485fe1c6df8601c71eb746212
commy2 <commy-2@gmx.de>
2019-03-30 20:51:37 +0100

    add 2d optics component

---
+++ addons/2doptics/script_component.hpp
@@ -0,0 +1,17 @@
+﻿#define COMPONENT 2doptics
+#include "\x\cba\addons\main\script_mod.hpp"
+
+//#define DEBUG_MODE_FULL
+//#define DISABLE_COMPILE_CACHE
+//#define DEBUG_ENABLED_2DOPTICS
+
+#ifdef DEBUG_ENABLED_2DOPTICS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_2DOPTICS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_2DOPTICS
+#endif
+
+#define DEBUG_SYNCHRONOUS
+#include "\x\cba\addons\main\script_macros.hpp"
