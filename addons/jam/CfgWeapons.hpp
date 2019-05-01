ab4f3a7bc576819377b53d7f3a0e6249c1dc12c6
PabstMirror <pabstmirror@gmail.com>
2019-04-30 22:37:53 -0500

    JAM - Add 1.92 vanilla mag wells (#1133)
    * JAM - Add 1.92 vanilla mag wells
    
    * add all mx mags to all mx rifles

--- addons/jam/CfgWeapons.hpp
+++ addons/jam/CfgWeapons.hpp
@@ -6,19 +6,19 @@ class CfgWeapons {
     class Rifle_Short_Base_F;
 
     class mk20_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_556x45_STANAG", "CBA_556x45_STANAG_L", "CBA_556x45_STANAG_XL", "CBA_556x45_STANAG_2D"};
+        magazineWell[] = {"STANAG_556x45", "CBA_556x45_STANAG", "CBA_556x45_STANAG_L", "CBA_556x45_STANAG_XL", "CBA_556x45_STANAG_2D"};
     };
     class SDAR_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_556x45_STANAG"};
+        magazineWell[] = {"STANAG_556x45", "CBA_556x45_STANAG"};
     };
     class Tavor_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_556x45_STANAG", "CBA_556x45_STANAG_L", "CBA_556x45_STANAG_XL", "CBA_556x45_STANAG_2D"};
+        magazineWell[] = {"STANAG_556x45", "CBA_556x45_STANAG", "CBA_556x45_STANAG_L", "CBA_556x45_STANAG_XL", "CBA_556x45_STANAG_2D"};
     };
     class arifle_SPAR_01_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_556x45_STANAG", "CBA_556x45_STANAG_L", "CBA_556x45_STANAG_XL", "CBA_556x45_STANAG_2D", "CBA_556x45_STANAG_2D_XL"};
+        magazineWell[] = {"STANAG_556x45", "CBA_556x45_STANAG", "CBA_556x45_STANAG_L", "CBA_556x45_STANAG_XL", "CBA_556x45_STANAG_2D", "CBA_556x45_STANAG_2D_XL"};
     };
     class arifle_SPAR_02_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_556x45_STANAG", "CBA_556x45_STANAG_L", "CBA_556x45_STANAG_XL", "CBA_556x45_STANAG_2D", "CBA_556x45_STANAG_2D_XL"};
+        magazineWell[] = {"STANAG_556x45_Large", "CBA_556x45_STANAG", "CBA_556x45_STANAG_L", "CBA_556x45_STANAG_XL", "CBA_556x45_STANAG_2D", "CBA_556x45_STANAG_2D_XL"};
     };
 
     class UGL_F: GrenadeLauncher {
@@ -26,56 +26,53 @@ class CfgWeapons {
     };
 
     class arifle_MX_Base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_65x39_MX"};
+        magazineWell[] = {"MX_65x39", "MX_65x39_Large", "CBA_65x39_MX", "CBA_65x39_MX_XL"};
         class GL_3GL_F: UGL_F {
             magazineWell[] = {"CBA_40mm_3GL", "CBA_40mm_M203", "CBA_40mm_EGLM"};
         };
     };
-    class arifle_MX_SW_F: arifle_MX_Base_F {
-        magazineWell[] = {"CBA_65x39_MX", "CBA_65x39_MX_XL"};
-    };
 
     class arifle_Katiba_Base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_65x39_Katiba"};
+        magazineWell[] = {"Katiba_65x39", "CBA_65x39_Katiba"};
     };
 
     class arifle_ARX_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_65x39_Katiba"};
+        magazineWell[] = {"Katiba_65x39", "CBA_65x39_Katiba"};
     };
 
     class EBR_base_F: Rifle_Long_Base_F {
-        magazineWell[] = {"CBA_762x51_M14"};
+        magazineWell[] = {"M14_762x51", "CBA_762x51_M14"};
     };
     class DMR_03_base_F: Rifle_Long_Base_F {
-        magazineWell[] = {"CBA_762x51_MkI_EMR"};
+        magazineWell[] = {"M14_762x51", "CBA_762x51_MkI_EMR"};
     };
     class DMR_06_base_F: Rifle_Long_Base_F {
-        magazineWell[] = {"CBA_762x51_M14"};
+        magazineWell[] = {"M14_762x51", "CBA_762x51_M14"};
     };
     class arifle_SPAR_03_base_F: Rifle_Base_F {
         magazineWell[] = {"CBA_762x51_HK417", "CBA_762x51_HK417_L", "CBA_762x51_HK417_XL"};
     };
 
     class DMR_01_base_F: Rifle_Long_Base_F {
-        magazineWell[] = {"CBA_762x54R_SVD"};
+        magazineWell[] = {"Rahim_762x54", "CBA_762x54R_SVD"};
     };
 
     class arifle_CTAR_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_580x42_TYPE95", "CBA_580x42_TYPE95_XL"};
+        magazineWell[] = {"CTAR_580x42", "CTAR_580x42_Large", "CBA_580x42_TYPE95", "CBA_580x42_TYPE95_XL"};
     };
     class arifle_CTARS_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_580x42_TYPE95", "CBA_580x42_TYPE95_XL"};
+        magazineWell[] = {"CTAR_580x42", "CTAR_580x42_Large", "CBA_580x42_TYPE95", "CBA_580x42_TYPE95_XL"};
     };
 
     class arifle_AK12_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_762x39_AK", "CBA_762x39_RPK"};
+        magazineWell[] = {"AK_762x39", "CBA_762x39_AK", "CBA_762x39_RPK"};
     };
     class arifle_AKM_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_762x39_AK", "CBA_762x39_RPK"};
+        magazineWell[] = {"AK_762x39", "CBA_762x39_AK", "CBA_762x39_RPK"};
     };
 
     class arifle_AKS_base_F: Rifle_Base_F {
-        magazineWell[] = {"CBA_545x39_AK", "CBA_545x39_RPK"};
+        magazineWell[] = {"AK_545x39", "CBA_545x39_AK", "CBA_545x39_RPK"};
     };
 
     class LMG_03_base_F: Rifle_Long_Base_F {
@@ -95,7 +92,7 @@ class CfgWeapons {
     };
 
     class launch_RPG7_F: Launcher_Base_F {
-        magazineWell[] = {"CBA_RPG7"};
+        magazineWell[] = {"RPG7", "CBA_RPG7"};
     };
 
     class MMG_01_base_F: Rifle_Long_Base_F {
