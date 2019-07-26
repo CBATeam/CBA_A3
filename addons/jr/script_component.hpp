20b432843cf1471c8d1d28bd98a6e785c444d790
commy2 <commy-2@gmx.de>
2019-07-26 09:14:52 +0200

    move some macros

--- addons/jr/script_component.hpp
+++ addons/jr/script_component.hpp
@@ -1,3 +1,12 @@
 #define COMPONENT jr
 #include "\x\cba\addons\main\script_mod.hpp"
 #include "\x\cba\addons\main\script_macros.hpp"
+
+#define private 0       // hidden
+#define protected 1     // hidden but usable
+#define public 2        // visible
+
+#define ReadAndWrite 0      //! any modifications enabled
+#define ReadAndCreate 1     //! only adding new class members is allowed
+#define ReadOnly 2          //! no modifications enabled
+#define ReadOnlyVerified 3  //! no modifications enabled, CRC test applied
