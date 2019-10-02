7af94f75bcce635b431acdd635042cf28a917c9e
NeilZar <neil.evers.1995@gmail.com>
2019-10-02 19:36:27 +0200

    Replace CBAr with CBAv
    Co-Authored-By: commy2 <commy-2@gmx.de>

--- addons/events/script_component.hpp
+++ addons/events/script_component.hpp
@@ -32,7 +32,7 @@
 #define SEND_TEVENT_TO_SERVER(params,name,targets) TEVENT_PVAR = [name, params, targets]; publicVariableServer TEVENT_PVAR_STR
 
 // turret events
-#define TUEVENT_PVAR CBAr
+#define TUEVENT_PVAR CBAv
 #define TUEVENT_PVAR_STR QUOTE(TUEVENT_PVAR)
 
 #define SEND_TUEVENT_TO_SERVER(params,name,vehicle,turret) TUEVENT_PVAR = [name, params, vehicle, turret]; publicVariableServer TUEVENT_PVAR_STR
