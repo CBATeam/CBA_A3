f976396eb7fe404d3e67ab526906e133c1bed5cc
commy2 <commy-2@gmx.de>
2020-03-14 12:59:57 +0100

    disable debug mode

--- addons/ui/script_component.hpp
+++ addons/ui/script_component.hpp
@@ -1,8 +1,8 @@
 #define COMPONENT ui
 #include "\x\cba\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
 // #define DEBUG_ENABLED_UI
 
 #ifdef DEBUG_ENABLED_UI
