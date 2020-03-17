1bb4371994ee587f5fad64cd6835b20325100a6c
jonpas <jonpas33@gmail.com>
2020-03-17 10:06:41 +0100

    Update HEMTT to newer development version

--- tools/setup.bat
+++ tools/setup.bat
@@ -12,7 +12,7 @@ function Unzip {
 $client = New-Object Net.WebClient
 
 Write-Output "=> Downloading HEMTT (Windows) ...`n"
-$client.DownloadFile("https://ci.appveyor.com/api/buildjobs/cy1n39shjje529li/artifacts/target%2Fx86_64-pc-windows-msvc%2Frelease%2Fhemtt.exe", "..\hemtt.exe")
+$client.DownloadFile("https://ci.appveyor.com/api/buildjobs/w02cqm8vq7rhjpy6/artifacts/target%2Fx86_64-pc-windows-msvc%2Frelease%2Fhemtt.exe", "..\hemtt.exe")
 $client.dispose()
 
 Write-Output "HEMTT successfully installed to project!`n"
