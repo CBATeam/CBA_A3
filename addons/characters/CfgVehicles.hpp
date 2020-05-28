41b747f4c9bc7ee61b23dd8e611675ae254df33f
oOKexOo <rc_mapper@yahoo.com>
2020-05-28 22:55:11 +0200

    Fix indentation

--- addons/characters/CfgVehicles.hpp
+++ addons/characters/CfgVehicles.hpp
@@ -26,51 +26,51 @@
 class CfgVehicles {
     class C_Man_casual_1_F_afro;
     class C_Man_casual_1_F_afro_sick: C_Man_casual_1_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_Man_casual_3_F_afro;
     class C_Man_casual_3_F_afro_sick: C_Man_casual_3_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_Man_casual_4_F_afro;
     class C_Man_casual_4_F_afro_sick: C_Man_casual_4_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_Man_casual_5_F_afro;
     class C_Man_casual_5_F_afro_sick: C_Man_casual_5_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_Man_casual_6_F_afro;
     class C_Man_casual_6_F_afro_sick: C_Man_casual_6_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_man_polo_1_F_afro;
     class C_man_polo_1_F_afro_sick: C_man_polo_1_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_man_polo_2_F_afro;
     class C_man_polo_2_F_afro_sick: C_man_polo_2_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_man_polo_3_F_afro;
     class C_man_polo_3_F_afro_sick: C_man_polo_3_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_man_polo_6_F_afro;
     class C_man_polo_6_F_afro_sick: C_man_polo_6_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 
     class C_man_sport_2_F_afro;
     class C_man_sport_2_F_afro_sick: C_man_sport_2_F_afro {
-       UNLOCK_MALARIA_INFECTED_CIVILIAN;
+        UNLOCK_MALARIA_INFECTED_CIVILIAN;
     };
 };
