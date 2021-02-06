1eaab5c4612657b45df732976cb016395766b6e6
Killswitch <killswitch@zeus-community.net>
2021-02-06 22:34:13 +0100

    check_strings: Pylint fixes
    * Use mixedCase variable and function naming style consistently
    * Added function docstrings
    * Various other Pylint quirks fixed
    
    Verified by running both with "-u" and without.

--- tools/check_strings.py
+++ tools/check_strings.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-# PabstMirror
-# Checks all strings are defined, run with -u to return all unused strings
+
+"""PabstMirror
+
+Checks that all strings are defined, run with -u to return all unused strings
+"""
 
 import fnmatch
 import os
@@ -8,72 +11,74 @@ import re
 import sys
 
 def getDefinedStrings(filepath):
+    """Return list of string names in a stringtable.xml file"""
     # print("getDefinedStrings {0}".format(filepath))
     with open(filepath, 'r', encoding="latin-1") as file:
         content = file.read()
-        srch = re.compile('Key ID\=\"(STR_CBA_[_a-zA-Z0-9]*)"', re.IGNORECASE)
+        srch = re.compile(r'Key ID\=\"(STR_CBA_[_a-zA-Z0-9]*)"', re.IGNORECASE)
         modStrings = srch.findall(content)
     modStrings = [s.lower() for s in modStrings]
     return modStrings
 
 def getStringUsage(filepath):
-    selfmodule = (re.search('addons[\W]*([_a-zA-Z0-9]*)', filepath)).group(1)
+    """Return list of string names used in a file"""
+    selfmodule = (re.search(r'addons[\W]*([_a-zA-Z0-9]*)', filepath)).group(1)
     # print("Checking {0} from {1}".format(filepath,selfmodule))
     fileStrings = []
 
     with open(filepath, 'r') as file:
         content = file.read()
 
-        srch = re.compile('(STR_CBA_[_a-zA-Z0-9]*)', re.IGNORECASE)
+        srch = re.compile(r'(STR_CBA_[_a-zA-Z0-9]*)', re.IGNORECASE)
         fileStrings = srch.findall(content)
 
-        srch = re.compile('[^E][CL]STRING\(([_a-zA-Z0-9]*)\)', re.IGNORECASE)
+        srch = re.compile(r'[^E][CL]STRING\(([_a-zA-Z0-9]*)\)', re.IGNORECASE)
         modStrings = srch.findall(content)
         for localString in modStrings:
             fileStrings.append("STR_CBA_{0}_{1}".format(selfmodule, localString))
 
-        srch = re.compile('E[CL]STRING\(([_a-zA-Z0-9]*),([_a-zA-Z0-9]*)\)')
+        srch = re.compile(r'E[CL]STRING\(([_a-zA-Z0-9]*),([_a-zA-Z0-9]*)\)')
         exStrings = srch.findall(content)
         for (exModule, exString) in exStrings:
             fileStrings.append("STR_CBA_{0}_{1}".format(exModule, exString))
 
-        srch = re.compile('IGNORE_STRING_WARNING\([\'"]*([_a-zA-Z0-9]*)[\'"]*\)')
+        srch = re.compile(r'IGNORE_STRING_WARNING\([\'"]*([_a-zA-Z0-9]*)[\'"]*\)')
         ignoreWarnings = srch.findall(content)
 
     fileStrings = [s.lower() for s in fileStrings]
     return [s for s in fileStrings if s not in (i.lower() for i in ignoreWarnings)]
 
-def main(argv):
+def main(argv): # pylint: disable=missing-function-docstring,too-many-branches
     print("### check_strings.py {} ###".format(argv))
-    sqf_list = []
-    xml_list = []
+    sqfList = []
+    xmlList = []
 
     allDefinedStrings = []
     allUsedStrings = []
 
     # Allow running from root directory as well as from inside the tools directory
     rootDir = "../addons"
-    if (os.path.exists("addons")):
+    if os.path.exists("addons"):
         rootDir = "addons"
 
-    for root, dirnames, filenames in os.walk(rootDir):
+    for root, _, filenames in os.walk(rootDir):
         for filename in fnmatch.filter(filenames, '*.sqf'):
-            sqf_list.append(os.path.join(root, filename))
+            sqfList.append(os.path.join(root, filename))
         for filename in fnmatch.filter(filenames, '*.cpp'):
-            sqf_list.append(os.path.join(root, filename))
+            sqfList.append(os.path.join(root, filename))
         for filename in fnmatch.filter(filenames, '*.hpp'):
             if filename != "script_macros_common.hpp":
-                sqf_list.append(os.path.join(root, filename))
+                sqfList.append(os.path.join(root, filename))
         for filename in fnmatch.filter(filenames, '*.h'):
-            sqf_list.append(os.path.join(root, filename))
+            sqfList.append(os.path.join(root, filename))
 
         for filename in fnmatch.filter(filenames, '*.xml'):
-            xml_list.append(os.path.join(root, filename))
+            xmlList.append(os.path.join(root, filename))
 
-    for filename in xml_list:
-        allDefinedStrings = allDefinedStrings + getDefinedStrings(filename)
-    for filename in sqf_list:
-        allUsedStrings = allUsedStrings + getStringUsage(filename)
+    for filename in xmlList:
+        allDefinedStrings.extend(getDefinedStrings(filename))
+    for filename in sqfList:
+        allUsedStrings.extend(getStringUsage(filename))
 
     allDefinedStrings = list(sorted(set(allDefinedStrings)))
     allUsedStrings = list(sorted(set(allUsedStrings)))
@@ -81,20 +86,22 @@ def main(argv):
     print("-----------")
     countUnusedStrings = 0
     countUndefinedStrings = 0
-    for s in allDefinedStrings:
-        if (not (s in allUsedStrings)):
-            countUnusedStrings = countUnusedStrings + 1;
-            if ("-u" in argv):
-                print("String {} defined but not used".format(s))
+    for string in allDefinedStrings:
+        if not string in allUsedStrings:
+            countUnusedStrings = countUnusedStrings + 1
+            if "-u" in argv:
+                print("String {} defined but not used".format(string))
     print("-----------")
-    for s in allUsedStrings:
-        if (not (s in allDefinedStrings)):
-            print("String {} not defined".format(s))
-            countUndefinedStrings = countUndefinedStrings + 1;
+    for string in allUsedStrings:
+        if not string in allDefinedStrings:
+            print("String {} not defined".format(string))
+            countUndefinedStrings = countUndefinedStrings + 1
     print("-----------")
 
-    print("Defined Strings:{0} Used Strings:{1}".format(len(allDefinedStrings),len(allUsedStrings)))
-    print("Unused Strings:{0} Undefined Strings:{1}".format(countUnusedStrings,countUndefinedStrings))
+    print("Defined Strings:{0} Used Strings:{1}".format(len(allDefinedStrings),
+                                                        len(allUsedStrings)))
+    print("Unused Strings:{0} Undefined Strings:{1}".format(countUnusedStrings,
+                                                            countUndefinedStrings))
 
     return countUndefinedStrings
 
