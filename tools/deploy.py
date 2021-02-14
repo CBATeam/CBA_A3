01406359ddb6a99208be763f03d1ce435292a476
Killswitch <killswitch@zeus-community.net>
2021-02-14 09:58:58 +0100

    deploy.py: Pylint fixes
    * Added docstrings.
    * Script can use either the Pygithub3 or PyGithub libraries.

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,54 +1,59 @@
 #!/usr/bin/env python3
 
+"""
 ####################################
 # CBA automatic deployment script  #
 # ================================ #
 # This is not meant to be run      #
 # directly!                        #
 ####################################
+"""
 
 import os
 import sys
-import shutil
 import traceback
 import subprocess as sp
-from pygithub3 import Github
+from subprocess import SubprocessError
+try:
+    from pygithub3 import Github
+except ImportError:
+    from github import Github
 
-
+# https://github.com/CBATeam/CBA_A3/issues/754
 TRANSLATIONISSUE = 754
-TRANSLATIONBODY = """**[CBA Translation Guide](https://github.com/CBATeam/CBA_A3/wiki/Translation-Guide)**
+TRANSLATIONBODY = "**[CBA Translation Guide]" \
+    "(https://github.com/CBATeam/CBA_A3/wiki/Translation-Guide)**\n" \
+    "\n{}"
 
-{}
-"""
 
 REPOUSER = "CBATeam"
 REPONAME = "CBA_A3"
-REPOPATH = "{}/{}".format(REPOUSER,REPONAME)
+REPOPATH = "{}/{}".format(REPOUSER, REPONAME)
 
 
 def update_translations(token):
+    """Update the CBA translation issue on GitHub"""
     diag = sp.check_output(["python3", "tools/stringtablediag.py", "--markdown"])
     diag = str(diag, "utf-8")
     repo = Github(token).get_repo(REPOPATH)
     issue = repo.get_issue(TRANSLATIONISSUE)
     issue.edit(body=TRANSLATIONBODY.format(diag))
 
-
-def main():
+def main(): # pylint: disable=missing-function-docstring
     print("Obtaining token ...")
     try:
         token = os.environ["GH_TOKEN"]
-    except:
+    except KeyError:
         print("Could not obtain token.")
         print(traceback.format_exc())
         return 1
     else:
-        print("Token sucessfully obtained.")
+        print("Token successfully obtained.")
 
     print("\nUpdating translation issue ...")
     try:
         update_translations(token)
-    except:
+    except (SubprocessError, Exception): # pylint: disable=broad-except
         print("Failed to update translation issue.")
         print(traceback.format_exc())
         return 1
