8e9bc465599fceead0d85bfecf0e37b6de31dae5
Jonas Hoffmann <hoffmann.jonas95@gmail.com>
2021-03-27 20:27:18 +0100

    use compileScript in script_macros_common.hpp

--- addons/main/script_macros_common.hpp
+++ addons/main/script_macros_common.hpp
@@ -809,7 +809,7 @@ Author:
 
 // This only works for binarized configs after recompiling the pbos
 // TODO: Reduce amount of calls / code..
-#define COMPILE_FILE2_CFG_SYS(var1) compile preprocessFileLineNumbers var1
+#define COMPILE_FILE2_CFG_SYS(var1) compileScript [var1]
 #define COMPILE_FILE2_SYS(var1) COMPILE_FILE2_CFG_SYS(var1)
 
 #define COMPILE_FILE_SYS(var1,var2,var3) COMPILE_FILE2_SYS('PATHTO_SYS(var1,var2,var3)')
@@ -960,8 +960,8 @@ Author:
 //#define PREP(var1) PREP_SYS(PREFIX,COMPONENT_F,var1)
 
 #ifdef DISABLE_COMPILE_CACHE
-    #define PREP(var1) TRIPLES(ADDON,fnc,var1) = compile preProcessFileLineNumbers 'PATHTO_SYS(PREFIX,COMPONENT_F,DOUBLES(fnc,var1))'
-    #define PREPMAIN(var1) TRIPLES(PREFIX,fnc,var1) = compile preProcessFileLineNumbers 'PATHTO_SYS(PREFIX,COMPONENT_F,DOUBLES(fnc,var1))'
+    #define PREP(var1) TRIPLES(ADDON,fnc,var1) = compileScript ['PATHTO_SYS(PREFIX,COMPONENT_F,DOUBLES(fnc,var1))']
+    #define PREPMAIN(var1) TRIPLES(PREFIX,fnc,var1) = compileScript ['PATHTO_SYS(PREFIX,COMPONENT_F,DOUBLES(fnc,var1))']
 #else
     #define PREP(var1) ['PATHTO_SYS(PREFIX,COMPONENT_F,DOUBLES(fnc,var1))', 'TRIPLES(ADDON,fnc,var1)'] call SLX_XEH_COMPILE_NEW
     #define PREPMAIN(var1) ['PATHTO_SYS(PREFIX,COMPONENT_F,DOUBLES(fnc,var1))', 'TRIPLES(PREFIX,fnc,var1)'] call SLX_XEH_COMPILE_NEW
