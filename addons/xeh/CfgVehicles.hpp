fbd247a5a079df38d2299c527d9d9623f45bc130
Björn Dahlgren <bjorn@dahlgren.io>
2021-10-18 13:10:42 +0200

    XEH - Fix issues with SOG PF

--- addons/xeh/CfgVehicles.hpp
+++ addons/xeh/CfgVehicles.hpp
@@ -427,4 +427,25 @@ class CfgVehicles {
     class GalleryDioramaUnit_01_base_F: Items_base_F {
         XEH_ENABLED;
     };
+
+    // SOG PF
+    class vn_object_b_base_02: Items_base_F {};
+    class Land_vn_candle_01: vn_object_b_base_02 {
+        XEH_ENABLED;
+    };
+
+    class vn_krait: Snake_random_F {
+        XEH_ENABLED;
+    };
+
+    class vn_parachute_02_base: ParachuteBase {};
+    class vn_b_parachute_02: vn_parachute_02_base {};
+    class vn_b_parachute_02_blu82: vn_b_parachute_02 {};
+    class vn_b_parachute_02_blu82_airdrop: vn_b_parachute_02_blu82 {
+        XEH_ENABLED;
+    };
+
+    class vn_o_static_rsna75_wreck: PlaneWreck {
+        XEH_ENABLED;
+    };
 };
