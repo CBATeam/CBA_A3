a6184a3db8c36c361448e08cb0a32f4e079003de
Björn Dahlgren <bjorn@dahlgren.io>
2021-11-18 15:39:11 +0100

    XEH - Fix issues with Western Sahara

--- addons/xeh/CfgVehicles.hpp
+++ addons/xeh/CfgVehicles.hpp
@@ -481,4 +481,67 @@ class CfgVehicles {
     class US85_M252_Stat: Mortar_01_base_F {
         XEH_ENABLED;
     };
+
+    // Western Sahara
+    class Land_PowerPoleWooden_lxWS: PowerLines_Small_base_F {
+        XEH_ENABLED;
+    };
+    class Land_PowerPoleWooden_L_lxWS: PowerLines_Small_base_F {
+        XEH_ENABLED;
+    };
+    class Land_PowerPoleWooden_L_off_lxWS: PowerLines_Small_base_F {
+        XEH_ENABLED;
+    };
+
+    class Site_Camels_lxWS: Module_F {
+        XEH_ENABLED;
+    };
+
+    class C_journalist_F;
+    class C_Journalist_lxWS: C_journalist_F {
+        scope = "1 + parseNumber isClass (configFile >> 'CfgPatches' >> 'data_f_lxWS')";
+        XEH_ENABLED;
+    };
+    
+    class C_Man_casual_1_F_afro;
+    class C_Tak_01_A_lxWS: C_Man_casual_1_F_afro {
+        scope = "1 + parseNumber isClass (configFile >> 'CfgPatches' >> 'data_f_lxWS')";
+        XEH_ENABLED;
+    };
+    
+    class B_Soldier_TL_F;
+    class B_ION_Story_Givens_lxWS: B_Soldier_TL_F {
+        scope = "1 + parseNumber isClass (configFile >> 'CfgPatches' >> 'data_f_lxWS')";
+        XEH_ENABLED;
+    };
+
+    class B_Soldier_F;
+    class I_PMC_Soldier_01_lxWS: B_Soldier_F {
+        scope = "1 + parseNumber isClass (configFile >> 'CfgPatches' >> 'data_f_lxWS')";
+    };
+    class I_PMC_Story_Gustavo_lxWS: I_PMC_Soldier_01_lxWS {
+        XEH_ENABLED;
+    };
+
+    class I_officer_F;
+    class I_SFIA_officer_lxWS: I_officer_F {
+        scope = "1 + parseNumber isClass (configFile >> 'CfgPatches' >> 'data_f_lxWS')";
+    };
+    class I_SFIA_Said_lxWS: I_SFIA_officer_lxWS {
+        XEH_ENABLED;
+    };
+
+    class Truck_02_base_F;
+    class Truck_02_cargo_base_lxWS: Truck_02_base_F {
+        XEH_ENABLED;
+    };
+    class Truck_02_box_base_lxWS: Truck_02_base_F {
+        XEH_ENABLED;
+    };
+    class Truck_02_Ammo_base_lxWS: Truck_02_base_F {
+        XEH_ENABLED;
+    };
+    class Truck_02_aa_base_lxWS: Truck_02_base_F {
+        XEH_ENABLED;
+    };
 };
