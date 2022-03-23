441ca74858a2afe8d547420d8abd886a6ebcf3d8
Björn Dahlgren <bjorn@dahlgren.io>
2022-03-23 01:27:35 +0100

    Add CBA magazine wells to Global Mobilization weapons

---
+++ addons/jam/jam_gm/config.cpp
@@ -0,0 +1,21 @@
+#if __has_include(\gm\gm_core\config.bin")
+
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = ECSTRING(jam,component);
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"cba_jam"};
+        author = "$STR_CBA_Author";
+        authors[] = {};
+        url = "$STR_CBA_URL";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
+
+#endif
