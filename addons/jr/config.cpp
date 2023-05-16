d2a10fb3c74150b6a390b2535c026a6d4d59fc01
jonpas <jonpas33@gmail.com>
2023-05-16 16:50:58 +0200

    JR - Remove private/protected/public/Read defines

--- addons/jr/config.cpp
+++ addons/jr/config.cpp
@@ -1,14 +1,5 @@
 #include "script_component.hpp"
 
-#define private 0
-#define protected 1
-#define public 2
-
-#define ReadAndWrite 0
-#define ReadAndCreate 1
-#define ReadOnly 2
-#define ReadOnlyVerified 3
-
 class CfgPatches {
     class ADDON {
         name = CSTRING(component);
