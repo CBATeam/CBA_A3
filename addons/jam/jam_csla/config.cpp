f9597346c3eda92833b0dcfb0233952e4d23d787
Björn Dahlgren <bjorn@dahlgren.io>
2023-09-07 20:31:28 +0200

    jam_csla

---
+++ addons/jam/jam_csla/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = ECSTRING(jam,component);
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cba_jam",
+            "CSLA"
+        };
+        author = "$STR_CBA_Author";
+        authors[] = {};
+        url = "$STR_CBA_URL";
+        skipWhenMissingDependencies = 1;
+        VERSION_CONFIG;
+    };
+};
