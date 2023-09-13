d9f4e732b6eb287a9659fac1ef43d2d6e04a47ba
PabstMirror <pabstmirror@gmail.com>
2023-09-12 20:56:57 -0500

    SUBADDON

--- addons/jam/jam_csla/script_component.hpp
+++ addons/jam/jam_csla/script_component.hpp
@@ -1,3 +1,2 @@
-#define COMPONENT jam_csla
-#include "\x\cba\addons\main\script_mod.hpp"
-#include "\x\cba\addons\main\script_macros.hpp"
+#define SUBCOMPONENT csla
+#include "..\script_component.hpp"
