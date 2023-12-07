634b47e872fd3e85851f78976f8818b144a3a4b1
PabstMirror <pabstmirror@gmail.com>
2023-12-06 22:41:15 -0600

    Tools - Don't compile 4 funcs that modify base functions

--- addons/diagnostic/CfgFunctions.hpp
+++ addons/diagnostic/CfgFunctions.hpp
@@ -19,7 +19,7 @@ class CfgFunctions {
     class A3 {
         class Debug {
             class isDebugConsoleAllowed {
-                file = QPATHTOF(fnc_isDebugConsoleAllowed.sqf);
+                file = QPATHTOF(fnc_isDebugConsoleAllowed.inc.sqf);
             };
         };
     };
