8bb83aa0283200070ff6c3da80a6e22dcf11b9f6
PabstMirror <pabstmirror@gmail.com>
2023-12-07 09:31:33 -0600

    Tools - Add `requiredVersion` to all sub configs (#1633)

--- addons/jr/asdg_jointrails/config.cpp
+++ addons/jr/asdg_jointrails/config.cpp
@@ -1,6 +1,9 @@
+#include "..\script_component.hpp"
+
 class CfgPatches {
     class asdg_jointrails {
         requiredAddons[] = {"cba_jr"};
         units[] = {};
+        requiredVersion = REQUIRED_VERSION;
     };
 };
