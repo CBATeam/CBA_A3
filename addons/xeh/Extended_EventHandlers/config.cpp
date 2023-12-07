24e82bed6cb69c594abf72b97bda95e2bcdb6968
PabstMirror <pabstmirror@gmail.com>
2023-12-06 23:10:02 -0600

    Update config.cpp

--- addons/xeh/Extended_EventHandlers/config.cpp
+++ addons/xeh/Extended_EventHandlers/config.cpp
@@ -1,6 +1,9 @@
+#include "..\script_component.hpp"
+
 class CfgPatches {
     class Extended_EventHandlers {
         requiredAddons[] = {"cba_xeh"};
         units[] = {};
+        requiredVersion = REQUIRED_VERSION;
     };
 };
