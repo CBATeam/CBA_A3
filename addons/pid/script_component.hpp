f6ee938c6af5e17e0ce0e486d0ad45f00bc20c2c
Bailey Danyluk <baileydanyluk@gmail.com>
2025-11-09 16:29:36 -0700

    fix somethings not updated

--- addons/pid/script_component.hpp
+++ addons/pid/script_component.hpp
@@ -2,14 +2,14 @@
 #include "\x\cba\addons\main\script_mod.hpp"
 
 // #define DISABLE_COMPILE_CACHE
-// #define STATEMACHINE_PERFORMANCE_COUNTERS
+// #define PID_PERFORMANCE_COUNTERS
 
 #ifdef DEBUG_ENABLED_PID
     #define DEBUG_MODE_FULL
 #endif
 
 #ifdef DEBUG_SETTINGS_PID
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_STATEMACHINE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_PID
 #endif
 
 #include "\x\cba\addons\main\script_macros.hpp"
